580d0a3666877f4cf41a58417c6a010ddcc2293e
joe2270 <joe227084@gmail.com>
2023-01-05 15:52:01 +0900

    기본적인 주소 및 설정 완료 반복문 작업 남음

--- job01_crawling.py
+++ job01_crawling.py
@@ -1,1 +1,17 @@
-print("hello world")
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+import pandas as pd
+import re
+import time
+import datetime
+
+option = webdriver.ChromeOptions()
+option.add_argument('lang=kr_KR')
+driver = webdriver.Chrome('C:/Users/ChoYJ/Desktop/Data_Study/Asia/1st_team_project(multi_classification)/Asia_Projcet01_multy-classification/chromedriver.exe', options=option)
+driver.get("https://novel.munpia.com/page/hd.platinum/group/pl.serial/view/serial")
+
+time.sleep(0.5)
+driver.find_element_by_xpath("""//*[@id="SECTION-MENU"]/ul/li[2]/a""").click()
+time.sleep(0.5)
+
+for i in 
