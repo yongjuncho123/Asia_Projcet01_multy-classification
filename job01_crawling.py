3e2c1e9a92eca7f9e618efef00fbcbee43b679c1
joe2270 <joe227084@gmail.com>
2023-01-09 10:54:44 +0900

    크롤링 끝

--- job01_crawling.py
+++ job01_crawling.py
@@ -10,12 +10,12 @@ option = webdriver.ChromeOptions()
 option.add_argument('lang=kr_KR')
 driver = webdriver.Chrome('C:/Users/ChoYJ/Desktop/Data_Study/Asia/1st_team_project(multi_classification)/Asia_Projcet01_multy-classification/chromedriver.exe', options=option)
 url = "https://novel.munpia.com/page/hd.platinum/group/pl.serial/finish/true/view/allend"
+category = category = ['판타지', '퓨전', '현대판타지', '무협', '스포츠', '대체역사', '로맨스', "SF", 'BL', '전쟁·밀리터리']
 
-time.sleep(0.5)
 
 driver.get(url)
 
-df_section = pd.DataFrame()
+df_novels = pd.DataFrame()
 for i in range(1,51):
     titles = []
     intros = []
@@ -43,8 +43,8 @@ for i in range(1,51):
                 pass
 
             try:
-                intro = driver.find_element_by_xpath(x_title).text
-                intro = re.compile('[^가-힣]').sub('', title) # re.compile: 한글만 추출 / sub: 문자열 치환
+                intro = driver.find_element_by_xpath(x_intro).text
+                intro = re.compile('[^가-힣]').sub('', intro) # re.compile: 한글만 추출 / sub: 문자열 치환
                 intros.append(intro) # 위에 만든 리스트에 넣기
             except NoSuchElementException as e:
                 time.sleep(0.5)
@@ -55,8 +55,8 @@ for i in range(1,51):
                 pass
 
             try:
-                genre = driver.find_element_by_xpath(x_title).text
-                genre = re.compile('[^가-힣]').sub('', title) # re.compile: 한글만 추출 / sub: 문자열 치환
+                genre = driver.find_element_by_xpath(x_genre).text
+                genre = re.compile('[^가-힣]').sub('', genre) # re.compile: 한글만 추출 / sub: 문자열 치환
                 genres.append(genre) # 위에 만든 리스트에 넣기
             except NoSuchElementException as e:
                 time.sleep(0.5)
@@ -72,7 +72,7 @@ for i in range(1,51):
         ### 1부터 5일때는 위에것 실행 그 이후 6으로 고정되는건 아래걸로
 
     else:
-        driver.find_element_by_xpath('//*[@id="NOVELOUS-CONTENTS"]/section[4]/ul/li[{}]/a'.format(i)).click() # 포멧 형태 {}.format(i)
+        driver.find_element_by_xpath('//*[@id="NOVELOUS-CONTENTS"]/section[4]/ul/li[6]/a'.format(i)).click() # 6으로 고정되니까 그대로
         time.sleep(2)
         for k in range(1, 51):
             driver.find_element_by_xpath('//*[@id="SECTION-LIST"]/ul/li[{}]/a[2]'.format(k)).click()
@@ -94,8 +94,8 @@ for i in range(1,51):
                 pass
 
             try:
-                intro = driver.find_element_by_xpath(x_title).text
-                intro = re.compile('[^가-힣]').sub('', title) # re.compile: 한글만 추출 / sub: 문자열 치환
+                intro = driver.find_element_by_xpath(x_intro).text
+                intro = re.compile('[^가-힣]').sub('', intro) # re.compile: 한글만 추출 / sub: 문자열 치환
                 intros.append(intro) # 위에 만든 리스트에 넣기
             except NoSuchElementException as e:
                 time.sleep(0.5)
@@ -106,8 +106,8 @@ for i in range(1,51):
                 pass
 
             try:
-                genre = driver.find_element_by_xpath(x_title).text
-                genre = re.compile('[^가-힣]').sub('', title) # re.compile: 한글만 추출 / sub: 문자열 치환
+                genre = driver.find_element_by_xpath(x_genre).text
+                genre = re.compile('[^가-힣]').sub('', genre) # re.compile: 한글만 추출 / sub: 문자열 치환
                 genres.append(genre) # 위에 만든 리스트에 넣기
             except NoSuchElementException as e:
                 time.sleep(0.5)
@@ -116,15 +116,20 @@ for i in range(1,51):
                 print(e)
             except:
                 pass
-            
-
+    time.sleep(1)        
+    print(titles)
+    print(intros)
+    print(genres)
     # 페이지 2번(총 100개 소설)을 크롤링 한 후 이것을 저장
     if i % 2 == 0:
-        df_section = pd.DataFrame({'titles':titles, 'intros': intros, 'genres': genres})
+        df_section = pd.DataFrame.from_dict({'titles':titles, 'intros': intros, 'genres': genres}, orient = 'index').T # valueerror: all arrays must be of the same length 해결
+        print(df_section)
+        # df_section = pd.DataFrame({'titles':titles, 'intros': intros, 'genres': genres})
         df_novels = pd.concat([df_novels, df_section], ignore_index= True)
-        df_novels = to_csv('./Moonpia_clawing_data_{}.csv'.format(i), index = False)
+        df_novels.to_csv('./Moonpia_clawing_data/Moonpia_clawing_data_{}.csv'.format(i), index = False)
+    time.sleep(1)
+    # df_section = pd.DataFrame({'titles':titles, 'intros': intros, 'genres': genres})
+    # df_novels = pd.concat([df_novels, df_section], ignore_index= True)
+    # df_novels.to_csv('./Moonpia_clawing_data/Moonpia_clawing_data_5.csv', index = False)
 
-    df_section = pd.DataFrame({'titles':titles, 'intros': intros, 'genres': genres})
-    df_novels = pd.concat([df_novels, df_section], ignore_index= True)
-    df_novels = to_csv('./Moonpia_clawing_data_5.csv', index = False)
-driver.close
+driver.close()
